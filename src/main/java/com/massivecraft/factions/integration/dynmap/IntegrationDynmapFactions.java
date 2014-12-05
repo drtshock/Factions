7d16ac73801c5b73472ccb3a2817810e46eb15a4
korikisulda <korikisulda@gmail.com>
2014-12-05 20:33:03 +0000

    Unnecessary classes are unnecessary

--- src/main/java/com/massivecraft/factions/integration/dynmap/IntegrationDynmapFactions.java
+++
@@ -1,7 +0,0 @@
-package com.massivecraft.factions.integration.dynmap;
-
-public class IntegrationDynmapFactions
-{
-	private static IntegrationDynmapFactions i = new IntegrationDynmapFactions();
-	public static IntegrationDynmapFactions get() { return i; }
-}
