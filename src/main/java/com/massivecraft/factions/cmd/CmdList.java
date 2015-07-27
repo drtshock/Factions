88db278814b4f7ce2d3f718aeed185f3b2cec180
Adam <packetdev@gmail.com>
2015-07-27 16:07:50 -0700

    Cache + async the /f list (WARNING: UNTESTED)

--- src/main/java/com/massivecraft/factions/cmd/CmdList.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdList.java
@@ -1,15 +1,20 @@
 package com.massivecraft.factions.cmd;
 
-import com.massivecraft.factions.Conf;
-import com.massivecraft.factions.Faction;
-import com.massivecraft.factions.Factions;
-import com.massivecraft.factions.struct.Permission;
-import com.massivecraft.factions.zcore.util.TL;
-import com.massivecraft.factions.zcore.util.TagUtil;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+import com.google.common.collect.Lists;
+import com.massivecraft.factions.Conf;
+import com.massivecraft.factions.Faction;
+import com.massivecraft.factions.Factions;
+import com.massivecraft.factions.P;
+import com.massivecraft.factions.struct.Permission;
+import com.massivecraft.factions.zcore.util.TL;
+import com.massivecraft.factions.zcore.util.TagUtil;
 
 
 public class CmdList extends FCommand {
@@ -36,7 +41,11 @@ public class CmdList extends FCommand {
         senderMustBeMember = false;
         senderMustBeModerator = false;
         senderMustBeAdmin = false;
+        this.lines = Lists.newArrayList();
     }
+
+    private List<String> lines;
+    private double lastCache;
 
     @Override
     public void perform() {
@@ -45,72 +54,89 @@ public class CmdList extends FCommand {
             return;
         }
 
-        ArrayList<Faction> factionList = Factions.getInstance().getAllFactions();
-        factionList.remove(Factions.getInstance().getWilderness());
-        factionList.remove(Factions.getInstance().getSafeZone());
-        factionList.remove(Factions.getInstance().getWarZone());
-
-        // Sort by total followers first
-        Collections.sort(factionList, new Comparator<Faction>() {
-            @Override
-            public int compare(Faction f1, Faction f2) {
-                int f1Size = f1.getFPlayers().size();
-                int f2Size = f2.getFPlayers().size();
-                if (f1Size < f2Size) {
-                    return 1;
-                } else if (f1Size > f2Size) {
-                    return -1;
-                }
-                return 0;
-            }
-        });
-
-        // Then sort by how many members are online now
-        Collections.sort(factionList, new Comparator<Faction>() {
-            @Override
-            public int compare(Faction f1, Faction f2) {
-                int f1Size = f1.getFPlayersWhereOnline(true).size();
-                int f2Size = f2.getFPlayersWhereOnline(true).size();
-                if (f1Size < f2Size) {
-                    return 1;
-                } else if (f1Size > f2Size) {
-                    return -1;
-                }
-                return 0;
-            }
-        });
-
-        ArrayList<String> lines = new ArrayList<String>();
-
-        factionList.add(0, Factions.getInstance().getWilderness());
-
-        final int pageheight = 9;
-        int pagenumber = this.argAsInt(0, 1);
-        int pagecount = (factionList.size() / pageheight) + 1;
-        if (pagenumber > pagecount) {
-            pagenumber = pagecount;
-        } else if (pagenumber < 1) {
-            pagenumber = 1;
-        }
-        int start = (pagenumber - 1) * pageheight;
-        int end = start + pageheight;
-        if (end > factionList.size()) {
-            end = factionList.size();
+        if (System.currentTimeMillis()-this.lastCache < 300000L) {
+        	sendMessage(this.lines);
+        	return;
         }
 
+        this.lastCache = System.currentTimeMillis();
 
-        String header = p.getConfig().getString("list.header", defaults[0]);
-        header = header.replace("{pagenumber}", String.valueOf(pagenumber)).replace("{pagecount}", String.valueOf(pagecount));
-        lines.add(p.txt.parse(header));
+        new BukkitRunnable() {
 
-        for (Faction faction : factionList.subList(start, end)) {
-            if (faction.isNone()) {
-                lines.add(p.txt.parse(TagUtil.parsePlain(faction, p.getConfig().getString("list.factionless", defaults[1]))));
-                continue;
-            }
-            lines.add(p.txt.parse(TagUtil.parsePlain(faction, fme, p.getConfig().getString("list.entry", defaults[2]))));
-        }
-        sendMessage(lines);
+			@Override
+			public void run() {
+				
+				ArrayList<Faction> factionList = Factions.getInstance().getAllFactions();
+		        factionList.remove(Factions.getInstance().getWilderness());
+		        factionList.remove(Factions.getInstance().getSafeZone());
+		        factionList.remove(Factions.getInstance().getWarZone());
+
+		        // Sort by total followers first
+		        Collections.sort(factionList, new Comparator<Faction>() {
+		            @Override
+		            public int compare(Faction f1, Faction f2) {
+		                int f1Size = f1.getFPlayers().size();
+		                int f2Size = f2.getFPlayers().size();
+		                if (f1Size < f2Size) {
+		                    return 1;
+		                } else if (f1Size > f2Size) {
+		                    return -1;
+		                }
+		                return 0;
+		            }
+		        });
+
+		        // Then sort by how many members are online now
+		        Collections.sort(factionList, new Comparator<Faction>() {
+		            @Override
+		            public int compare(Faction f1, Faction f2) {
+		                int f1Size = f1.getFPlayersWhereOnline(true).size();
+		                int f2Size = f2.getFPlayersWhereOnline(true).size();
+		                if (f1Size < f2Size) {
+		                    return 1;
+		                } else if (f1Size > f2Size) {
+		                    return -1;
+		                }
+		                return 0;
+		            }
+		        });
+
+		        lines = Lists.newArrayList();
+
+		        factionList.add(0, Factions.getInstance().getWilderness());
+
+		        final int pageheight = 9;
+		        int pagenumber = argAsInt(0, 1);
+		        int pagecount = (factionList.size() / pageheight) + 1;
+		        if (pagenumber > pagecount) {
+		            pagenumber = pagecount;
+		        } else if (pagenumber < 1) {
+		            pagenumber = 1;
+		        }
+		        int start = (pagenumber - 1) * pageheight;
+		        int end = start + pageheight;
+		        if (end > factionList.size()) {
+		            end = factionList.size();
+		        }
+
+
+		        String header = p.getConfig().getString("list.header", defaults[0]);
+		        header = header.replace("{pagenumber}", String.valueOf(pagenumber)).replace("{pagecount}", String.valueOf(pagecount));
+		        lines.add(p.txt.parse(header));
+
+		        for (Faction faction : factionList.subList(start, end)) {
+		            if (faction.isNone()) {
+		                lines.add(p.txt.parse(TagUtil.parsePlain(faction, p.getConfig().getString("list.factionless", defaults[1]))));
+		                continue;
+		            }
+		            lines.add(p.txt.parse(TagUtil.parsePlain(faction, fme, p.getConfig().getString("list.entry", defaults[2]))));
+		        }
+		        sendMessage(lines);
+
+			}
+
+        }.runTaskAsynchronously(P.p);
+
     }
 
     @Override
