210494cf601cade435033fab43da4a64b036f46d
Dariasc <dariascauas@gmail.com>
2018-02-01 17:27:48 -0300

    Remove unused import

--- src/main/java/com/massivecraft/factions/listeners/FactionsPlayerListener.java
+++ src/main/java/com/massivecraft/factions/listeners/FactionsPlayerListener.java
@@ -23,7 +23,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.*;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.util.NumberConversions;
