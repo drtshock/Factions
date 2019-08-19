b2c13b5b36599fa8002ae117202adfdd5ff8dc21
uf0h <3p6824cbrf@gmail.com>
2019-08-19 17:04:03 +0100

    /f delhome implementation

--- src/main/java/com/massivecraft/factions/util/TL.java
+++ src/main/java/com/massivecraft/factions/util/TL.java
@@ -191,6 +191,9 @@ public enum TL {
     COMMAND_DELFWARP_FORDELETE("for deleting warp"),
     COMMAND_DELFWARP_DESCRIPTION("Delete a faction warp"),
 
+    COMMAND_DELHOME_DEL("%1$s&e unset the home for your faction."),
+    COMMAND_DELHOME_DESCRIPTION("Unset the faction home"),
+
     COMMAND_DESCRIPTION_CHANGES("You have changed the description for &d%1$s&e to:"),
     COMMAND_DESCRIPTION_CHANGED("&eThe faction %1$s&e changed their description to:"),
     COMMAND_DESCRIPTION_TOCHANGE("to change faction description"),
